45cca4dec68a5b25dbf6d06630f41594c34682bb
William Chou <williamchoudev@gmail.com>
2019-05-17 00:12:09 -0400

    Add Publisher type and query

--- schema/schema.js
+++ schema/schema.js
@@ -1,29 +1,61 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLID
+} = graphql;
+
+const games = [
+  { name: 'Skyrim', genres: ['Fantasy', 'Action'], id: '1' },
+  { name: 'Overlord', genres: ['Fantasy', 'RPG'], id: '2' }
+];
+
+const publishers = [
+  { name: 'Bethesda Softworks', id: '1' },
+  { name: 'Codemasters', id: '2' }
+];
 
 const GameType = new GraphQLObjectType({
   name: 'Game',
   fields: () => ({
-    id: { type: GraphQLString},
-    name: { type: GraphQLString},
-    genres: { type: new GraphQLList(GraphQLString)}
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genres: { type: new GraphQLList(GraphQLString) }
   })
-})
+});
+
+const PublisherType = new GraphQLObjectType({
+  name: 'Publisher',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
     game: {
       type: GameType,
-      args: { id: { type: GraphQLString}},
-      resolve(parent, args) {
-
+      args: { id: { type: GraphQLID } },
+      resolve(parent, { id }) {
+        return _.find(games, { id });
+      }
+    },
+    publisher: {
+      type: PublisherType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, { id }) {
+        return _.find(publishers, { id });
       }
     }
   }
-})
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery
-})
+});
